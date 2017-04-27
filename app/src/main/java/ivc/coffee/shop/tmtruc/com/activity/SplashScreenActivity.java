895dfa2ec02141843dc92312e084da82d2e070b8
tmt <email>
2017-04-27 21:43:31 +0700

    commit 2

--- app/src/main/java/ivc/coffee/shop/tmtruc/com/activity/SplashScreenActivity.java
+++ app/src/main/java/ivc/coffee/shop/tmtruc/com/activity/SplashScreenActivity.java
@@ -8,7 +8,7 @@ import ivc.coffee.shop.tmtruc.com.R;
 
 public class SplashScreenActivity extends AppCompatActivity {
 
-    private static int SPLASH_TIME_OUT = 5000;
+    private static int SPLASH_TIME_OUT = 500;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
--- app/src/main/java/ivc/coffee/shop/tmtruc/com/fragment/HomeFragment.java
+++ app/src/main/java/ivc/coffee/shop/tmtruc/com/fragment/HomeFragment.java
@@ -8,6 +8,11 @@ import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.view.animation.AnimationUtils;
+import android.widget.ImageView;
+import android.widget.ViewFlipper;
+
+import com.squareup.picasso.Picasso;
 
 import java.util.ArrayList;
 import java.util.Timer;
@@ -26,6 +31,7 @@ public class HomeFragment extends Fragment {
     ArrayList<Integer> imageList;
     ViewPagerAdapter viewPagerAdapter;
     Timer timer;
+    ViewFlipper viewFlipper;
 
 
     public HomeFragment() {
@@ -38,46 +44,63 @@ public class HomeFragment extends Fragment {
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         View view = inflater.inflate(R.layout.fragment_home, container, false);
-        viewPager = (ViewPager) view.findViewById(R.id.viewpager_slide_image);
+//        viewPager = (ViewPager) view.findViewById(R.id.viewpager_slide_image);
 
         imageList = new ArrayList<>();
 
         imageList.add(R.drawable.coffee_shop_img_1);
         imageList.add(R.drawable.coffee_shop_img_2);
         imageList.add(R.drawable.coffee_shop_img_3);
+        imageList.add(R.drawable.coffee_shop_img_4);
 
-        viewPagerAdapter = new ViewPagerAdapter(getContext(), imageList);
-        viewPager.setAdapter(viewPagerAdapter);
+        viewFlipper = (ViewFlipper) view.findViewById(R.id.viewflipper);
+        for (int i = 0; i<imageList.size(); i++){
+            ImageView imageView = new ImageView(getContext());
+            imageView.setLayoutParams(new ViewGroup.LayoutParams(ViewGroup.LayoutParams.WRAP_CONTENT, ViewGroup.LayoutParams.WRAP_CONTENT));
+            imageView.setAdjustViewBounds(true);
+            Picasso.with(getContext()).load(imageList.get(i)).into(imageView);
+            viewFlipper.addView(imageView);
+        }
+        viewFlipper.setFlipInterval(5000);
+        viewFlipper.setInAnimation(AnimationUtils.loadAnimation(getContext(), android.R.anim.slide_in_left));
+        viewFlipper.setOutAnimation(AnimationUtils.loadAnimation(getContext(), android.R.anim.slide_out_right));
+        viewFlipper.setAutoStart(true);
 
-        timer = new Timer();
-        timer.scheduleAtFixedRate(new MyTimerTask(), 5000, 5000);
+
+
+
+//        viewPagerAdapter = new ViewPagerAdapter(getContext(), imageList);
+//        viewPager.setAdapter(viewPagerAdapter);
+//
+//        timer = new Timer();
+//        timer.scheduleAtFixedRate(new MyTimerTask(), 5000, 5000);
         return view;
     }
 
-    @Override
-    public void onDestroy() {
-        super.onDestroy();
-        if (timer != null) {
-            timer.cancel();
-        }
-    }
+//    @Override
+//    public void onDestroy() {
+//        super.onDestroy();
+//        if (timer != null) {
+//            timer.cancel();
+//        }
+//    }
 
-    public class MyTimerTask extends TimerTask {
-
-        @Override
-        public void run() {
-
-            getActivity().runOnUiThread(new Runnable() {
-                @Override
-                public void run() {
-                    if (viewPager.getCurrentItem() == viewPagerAdapter.getCount() - 1) {
-                        viewPager.setCurrentItem(0);
-                    } else {
-                        viewPager.setCurrentItem(viewPager.getCurrentItem() + 1);
-                    }
-                }
-            });
-
-        }
-    }
+//    public class MyTimerTask extends TimerTask {
+//
+//        @Override
+//        public void run() {
+//
+//            getActivity().runOnUiThread(new Runnable() {
+//                @Override
+//                public void run() {
+//                    if (viewPager.getCurrentItem() == viewPagerAdapter.getCount() - 1) {
+//                        viewPager.setCurrentItem(0);
+//                    } else {
+//                        viewPager.setCurrentItem(viewPager.getCurrentItem() + 1);
+//                    }
+//                }
+//            });
+//
+//        }
+//    }
 }
